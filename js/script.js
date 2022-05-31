76c027d1db7d40653e0ed17b483d4f0ddf097596
snholmes <shelbynholmnes@gmail.com>
2022-05-31 10:36:12 -0700

    Updated menu, index, order pages

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Menu tab navigation
-function openCity(evt, cityName) {
+function openMenu(evt, sectionMenu) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -9,7 +9,7 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(sectionMenu).style.display = "block";
     evt.currentTarget.className += " active";
   }
 
